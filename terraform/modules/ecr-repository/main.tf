64a208c44a69fb5167554c3a99320d13f981e712
attila-g-kocsis <attila.g.kocsis@gmail.com>
2026-06-06 12:42:58 +0200

    ecr name change

--- terraform/modules/ecr-repository/main.tf
+++ terraform/modules/ecr-repository/main.tf
@@ -1,11 +1,10 @@
 resource "aws_ecr_repository" "ecr" {
-  name                 = local.name
+  name                 = var.name
   image_tag_mutability = var.image_tag_mutability
-  dynamic "image_scanning_configuration" {
-    for_each = var.image_scanning_configuration
-    content {
-      scan_on_push = var.image_scanning_configuration.scan_on_push
-    }
+
+  image_scanning_configuration {
+    scan_on_push = var.image_scanning_configuration.scan_on_push
   }
+
   tags = var.tags
 }
